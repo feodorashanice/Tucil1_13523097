dca69776e6327e63d94ddd1fec65184634ff586e
Shanice Feodora Tjahjono <19623008@mahasiswa.itb.ac.id>
2025-02-21 08:48:51 +0700

    fix: add conditions in place and remove

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,7 @@ public class Board {
     private int N, M;
 
     // Constructor
-    public Board(int N, int M) {
+    public Board(int N, int M){
         this.N = N;
         this.M = M;
         board = new char[N][M];
@@ -16,13 +16,13 @@ public class Board {
         }
     }
     // Boolean to check if a piece can be placed on the board (position is not out of bounds and empty)
-    public boolean placeValid(char[][] piece, int row, int col) {
-        if (row + piece.length > N || col + piece[0].length > M) {
+    public boolean placeValid(char[][] piece, int row, int col){
+        if (row + piece.length > N || col + piece[0].length > M){
             return false;
         } // False due to out of bounds
-        for (int i = 0; i < piece.length; i++) {
-            for (int j = 0; j < piece[0].length; j++) {
-                if (piece[i][j] != '.' && board[row + i][col + j] != '.') {
+        for (int i = 0; i < piece.length; i++){
+            for (int j = 0; j < piece[0].length; j++){
+                if (piece[i][j] != '.' && board[row + i][col + j] != '.'){
                     return false;   
                 } // False due to overlapping pieces
             }
@@ -30,33 +30,30 @@ public class Board {
         return true;
     }
     // Place a piece on the board
-    public void placePiece(char[][] piece, int row, int col) {
-        for (int i = 0; i < piece.length; i++) {
-            for (int j = 0; j < piece[0].length; j++) {
-                if (piece[i][j] != '.') {
+    public void placePiece(char[][] piece, int row, int col){
+        if (!placeValid(piece, row, col)) return;
+        for (int i = 0; i < piece.length; i++){
+            for (int j = 0; j < piece[0].length; j++){
+                if (row + i < N && col + j < M && piece[i][j] != '.'){
                     board[row + i][col + j] = piece[i][j];
                 }
             }
         }
     }
     // Remove a piece from the board
-    public void removePiece(char[][] piece, int row, int col) {
-        for (int i = 0; i < piece.length; i++) {
-            for (int j = 0; j < piece[0].length; j++) {
-                if (piece[i][j] != '.') {
+    public void removePiece(char[][] piece, int row, int col){
+        for (int i = 0; i < piece.length; i++){
+            for (int j = 0; j < piece[0].length; j++){
+                if (row + i < N && col + j < M && piece[i][j] != '.'){
                     board[row + i][col + j] = '.';
                 }
             }
         }
     }
     // Print the board
-    public void printBoard() {
-        for (char[] row : board) {
+    public void printBoard(){
+        for (char[] row : board){
             System.out.println(new String(row));
         }
-    }
-    // Get board
-    public char[][] getBoard() {
-        return board;
     }
 }
