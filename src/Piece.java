b31b22847094da8427fdff190ea0b03aca748644
Shanice Feodora Tjahjono <19623008@mahasiswa.itb.ac.id>
2025-02-21 09:24:14 +0700

    fix: make piece its own class

---
+++ src/Piece.java
@@ -0,0 +1,43 @@
+public class Piece{
+    private char[][] piece;
+    private char id;
+
+    // Constructor
+    public Piece(char[][] piece, char id){
+        this.piece = piece;
+        this.id = id;
+    }
+
+    // Getter of piece shape copy
+    public char[][] getPiece(){
+        int rows = piece.length, cols = piece[0].length;
+        char[][] copy = new char[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                copy[i][j] = piece[i][j];
+        return copy;
+    }
+
+    // Getter of piece ID
+    public char getID(){
+        return id;
+    }
+
+    private char[][] rotate90() {
+        int rows = piece.length, cols = piece[0].length;
+        char[][] rotated = new char[cols][rows];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                rotated[j][rows - 1 - i] = piece[i][j];
+        return rotated;
+    }
+
+    private char[][] flipHorizontal() {
+        int rows = piece.length, cols = piece[0].length;
+        char[][] flipped = new char[rows][cols];
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                flipped[i][cols - 1 - j] = piece[i][j];
+        return flipped;
+    }
+}
