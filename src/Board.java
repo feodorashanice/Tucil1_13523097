01157067b40bbc0526e79a6eb54ee8b61be7f0ad
Shanice Feodora Tjahjono <19623008@mahasiswa.itb.ac.id>
2025-02-24 07:52:34 +0700

    feat: add color to output

--- src/Board.java
+++ src/Board.java
@@ -23,6 +23,9 @@ public class Board {
     public int getM(){
         return M;
     }
+    public char[][] getBoard(){
+        return board;
+    }
     
     // Boolean to check if a piece can be placed on the board (position is not out of bounds and empty)
     public boolean placeValid(char[][] piece, int row, int col){
@@ -70,10 +73,50 @@ public class Board {
         }
         return true;
     }
-    // Print the board
     public void printBoard(){
         for (char[] row : board){
-            System.out.println(new String(row));
+            StringBuilder line = new StringBuilder();
+            for (char cell : row){
+                if (cell == '.'){
+                    line.append(cell).append(" ");
+                } else{
+                    String color = getColor(cell);
+                    line.append(color).append(cell).append("\u001B[0m ").append(" ");
+                }
+            }
+            System.out.println(line.toString());
+        }
+    }
+    
+    private String getColor(char id){
+        switch (id){
+            case 'A': return "\u001B[31m"; // Red
+            case 'B': return "\u001B[32m"; // Green
+            case 'C': return "\u001B[33m"; // Yellow
+            case 'D': return "\u001B[34m"; // Blue
+            case 'E': return "\u001B[35m"; // Magenta
+            case 'F': return "\u001B[36m"; // Cyan
+            case 'G': return "\u001B[37m"; // White
+            case 'H': return "\u001B[91m"; // Bright Red
+            case 'I': return "\u001B[92m"; // Bright Green
+            case 'J': return "\u001B[93m"; // Bright Yellow
+            case 'K': return "\u001B[94m"; // Bright Blue
+            case 'L': return "\u001B[95m"; // Bright Magenta
+            case 'M': return "\u001B[96m"; // Bright Cyan
+            case 'N': return "\u001B[97m"; // Bright White
+            case 'O': return "\u001B[30m"; // Black
+            case 'P': return "\u001B[90m"; // Dark Gray
+            case 'Q': return "\u001B[40m"; // Background Black
+            case 'R': return "\u001B[41m"; // Background Red
+            case 'S': return "\u001B[42m"; // Background Green
+            case 'T': return "\u001B[43m"; // Background Yellow
+            case 'U': return "\u001B[44m"; // Background Blue
+            case 'V': return "\u001B[45m"; // Background Magenta
+            case 'W': return "\u001B[46m"; // Background Cyan
+            case 'X': return "\u001B[47m"; // Background White
+            case 'Y': return "\u001B[100m"; // Background Dark Gray
+            case 'Z': return "\u001B[101m"; // Background Bright Red
+            default: return "\u001B[0m"; // Default (no color)
         }
     }
 }
